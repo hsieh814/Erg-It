5fe4fe2a85eb5f675409734504e2d603f548e789
Lena Hsieh <lena_hsieh@hotmail.com>
2015-06-15 23:17:31 -0400

    initial commit

---
+++ Erg-It/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Erg-It
+//
+//  Created by Lena Hsieh on 2015-06-15.
+//  Copyright (c) 2015 lhsieh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
